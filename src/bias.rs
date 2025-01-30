e2628eabd64c02797cf21fc6e5f7b5ab79535750
Markus Klein <markus-klein@live.de>
2025-01-30 15:07:01 +0100

    refactor: Move random_play to bias module

--- src/bias.rs
+++ src/bias.rs
@@ -1,6 +1,6 @@
-use rand::Rng;
+use rand::{seq::IndexedRandom as _, Rng};
 
-use crate::{random_play, Evaluation, TwoPlayerGame};
+use crate::{Count, Evaluation, GameState, TwoPlayerGame};
 
 /// Used to obtain an ininitial bias for the outcome of a game starting from a given board.
 pub trait Bias<G: TwoPlayerGame> {
@@ -16,5 +16,42 @@ where
 {
     fn bias(&self, game: G, move_buf: &mut Vec<G::Move>, rng: &mut impl Rng) -> Evaluation {
         Evaluation::Undecided(random_play(game, move_buf, rng))
+    }
+}
+
+/// Play random moves, until the game is over and report the score from the perspective of the
+/// player whose turn it is.
+pub fn random_play<G>(mut game: G, moves_buf: &mut Vec<G::Move>, rng: &mut impl Rng) -> Count
+where
+    G: TwoPlayerGame,
+{
+    loop {
+        match game.state(moves_buf) {
+            GameState::Moves(legal_moves) => {
+                let selected_move = legal_moves.choose(rng).unwrap();
+                game.play(selected_move)
+            }
+            GameState::WinPlayerOne => {
+                break Count {
+                    wins_player_one: 1,
+                    wins_player_two: 0,
+                    draws: 0,
+                }
+            }
+            GameState::WinPlayerTwo => {
+                break Count {
+                    wins_player_one: 0,
+                    wins_player_two: 1,
+                    draws: 0,
+                }
+            }
+            GameState::Draw => {
+                break Count {
+                    wins_player_one: 0,
+                    wins_player_two: 0,
+                    draws: 1,
+                }
+            }
+        }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,11 @@
 mod bias;
 mod evaluation;
 mod player;
-mod simulation;
 mod tree;
 mod two_player_game;
 
-use self::simulation::random_play;
-
 pub use self::{
-    bias::{Bias, RandomPlayoutBias},
+    bias::{random_play, Bias, RandomPlayoutBias},
     evaluation::{Count, Evaluation},
     player::Player,
     tree::Tree,
